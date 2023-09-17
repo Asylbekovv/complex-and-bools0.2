f9d2ab6442906122415cc42b3428af53d431bbe6
Asylbekovv <144426976+Asylbekovv@users.noreply.github.com>
2023-09-18 00:40:37 +0600

    Add files via upload

---
+++ bool.py
@@ -0,0 +1,20 @@
+db_is_available = True
+print(db_is_available)
+print(type(db_is_available))
+db_is_available = False
+print(db_is_available)
+
+print(bool(112))
+print(bool("Arstan"))
+print(bool(33.12))
+print(bool(None))
+print(bool([112, 3345]))
+print(bool({}))
+
+print(200 > 44)
+print("Long string for python" == "Long string for python")
+print({"a": 22}, {"b": 44}, {"c": 88}, {"d": 66})
+print([123] == [321])
+
+
+
---
+++ complex_int.py
@@ -0,0 +1,8 @@
+complex_a = 20 + 88j
+complex_b = 39 + 90j
+complex_c = 34 + 77j
+complex_d = 66 + 21j
+sum = complex_a + complex_b + complex_c + complex_d
+print(sum)
+print(type(sum))
+
